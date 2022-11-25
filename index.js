4c027bd468059a42300ca4fc008e7ba3d37b1c00
alimransahin <mdalimransahin@gmail.com>
2022-11-25 15:21:12 +0600

    server setup complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const packageName = require('packageName');
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const port=process.env.PORT || 5000;
+const app=express();
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@cluster0.vgokw2y.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+app.get('/',(req,res)=>{
+    res.send('bbookss server is running');
+})
+
+app.listen(port,()=>console.log(`bbookss server running on port: ${port}`))
