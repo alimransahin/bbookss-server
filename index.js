35406139bf31e11a689b944c2fbebcb404cfb469
alimransahin <mdalimransahin@gmail.com>
2022-11-25 17:20:03 +0600

    categories api done

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+require('dotenv').config();
 const port=process.env.PORT || 5000;
 const app=express();
 
 app.use(cors());
 app.use(express.json());
 
-
-const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@cluster0.vgokw2y.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_password}@cluster0.vgokw2y.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        const categoriesCollection = client.db('bbookss').collection('categories');
+        app.get('/categories', async(req,res)=>{
+            const query={};
+            const result = await categoriesCollection.find(query).toArray();
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
 
 app.get('/',(req,res)=>{
     res.send('bbookss server is running');
